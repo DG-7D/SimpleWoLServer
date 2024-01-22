7a5965c382ad19ada29131a0e3121229be5cac3e
DG-7D <96519093+DG-7D@users.noreply.github.com>
2024-01-22 22:56:11 +0900

    整理した

---
+++ src/generator.ts
@@ -0,0 +1,41 @@
+import fs from "fs";
+
+const generated: { [path: string]: string } = {
+    "/index.html": indexHtml(),
+}
+
+export default {
+    generated: generated,
+}
+
+function indexHtml() {
+    const configFile = process.env["WOL_CONFIG"];
+    let template = fs.readFileSync("./public/index.template.html", "utf-8");
+    const settings: { name: string, macAddress?: string, ping?: string, services?: { name: string, url: string }[] }[] = JSON.parse(fs.readFileSync(configFile!, "utf-8"));
+    let generated = "";
+
+    for (const device of settings) {
+        generated += `
+        <tr>
+            <td class="ping" ${device.ping ? "data-ping=" + device.ping : ""}>❓</td>
+            <td>${device.name}</td>
+            <td>${device.macAddress ? "<button class=\"wake\" data-mac-address=\"" + device.macAddress + "\">Wake</button>" : ""}</td>
+            <td>
+                <ul>
+    `
+        if (device.services) {
+            for (const service of device.services) {
+                generated += `
+                    <li><a href="${service.url}">${service.name}</a></li>
+            `
+            }
+            generated += `
+                </ul>
+            </td>   
+        </tr>
+        `
+        }
+    }
+
+    return template.replace("<!-- generate -->", generated);
+}
--- src/server.ts
+++ src/server.ts
@@ -1,50 +1,24 @@
-import http from "http";
 import fs from "fs";
-import dgram from "dgram";
+import http from "http";
 import path from "path";
-import { execSync } from "child_process";
+
+import api from "./api"
+import generator from "./generator";
+import mimeTypes from "./mimeTypes";
 
 const port = 3000;
-const pingTimeout = 1000;
-
-const basePath = "./dist/static"
-const mimeTypes: { [key: string]: string } = {
-    ".html": "text/html",
-    ".js": "text/javascript",
-    ".css": "text/css",
-    ".json": "application/json",
-    ".txt": "text/plain",
-};
+const staticPath = "./public/static"
 
 http.createServer((request, response) => {
-
     console.log(`${request.method} ${request.url}`)
 
     if (request.url?.startsWith("/api")) {
-        processApi(request, response);
+        api.response(request, response);
         return;
     }
 
     if (request.method == "GET") {
-        let filePath = basePath + request.url;
-        filePath.endsWith("/") && (filePath += "index.html");
-        console.log(`=> ${filePath}`);
-
-        const extention = path.extname(filePath).toLowerCase();
-        const contentType = mimeTypes[extention];
-
-        fs.readFile(filePath, "utf-8", (error, content) => {
-            if (!error) {
-                response.writeHead(200, { "Content-Type": contentType });
-                response.end(content);
-                return;
-            }
-            if (error.code != "ENOENT") {
-                response.writeHead(500);
-                response.end();
-                return;
-            }
-        });
+        webResponse(request, response);
         return;
     }
 
@@ -56,85 +30,33 @@ http.createServer((request, response) => {
         console.log(`Server runnning at http://localhost:${port}/`);
     });
 
-function processApi(request: http.IncomingMessage, response: http.ServerResponse) {
-    const requestURL = new URL(request.url ?? "", `http://${request.headers.host}`);
-    if (request.method == "GET" && requestURL.pathname == "/api/ping") {
-        const hostname = requestURL.searchParams.get("hostname");
-        if (hostname) {
-            response.writeHead(200, { "content-type": mimeTypes[".json"] })
-            response.end(JSON.stringify({
-                responsed: ping(hostname),
-            }));
+function webResponse(request: http.IncomingMessage, response: http.ServerResponse) {
+    let filePath = request.url ?? "";
+    if (filePath.endsWith("/")) filePath += "index.html";
+
+    const extention = path.extname(filePath).toLowerCase();
+    const contentType = mimeTypes[extention];
+
+    if (generator.generated[filePath]) {
+        response.writeHead(200, { "Content-Type": contentType });
+        response.end(generator.generated[filePath]);
+        return;
+    }
+
+    filePath = staticPath + filePath;
+    fs.readFile(filePath, "utf-8", (error, content) => {
+        if (!error) {
+            response.writeHead(200, { "Content-Type": contentType });
+            response.end(content);
             return;
         }
-        response.writeHead(400, { "content-type": mimeTypes[".txt"] });
-        response.end("bad request: missig `hostname`");
+        if (error.code != "ENOENT") {
+            response.writeHead(500);
+            response.end();
+            return;
+        }
+        response.writeHead(404);
+        response.end();
         return;
-    }
-    if (request.method == "POST" && requestURL.pathname == "/api/wake") {
-        let requestBody = "";
-        request.on("data", chunk => {
-            requestBody += chunk.toString();
-        });
-        request.on("end", () => {
-            try {
-                const requestJson = JSON.parse(requestBody);
-                if (requestJson.macAddress) {
-                    wake(requestJson.macAddress);
-                    response.writeHead(200);
-                    response.end();
-                    return;
-                }
-                response.writeHead(400, { "content-type": mimeTypes[".txt"] });
-                response.end("bad request: missing `macAddress`");
-                return;
-            } catch {
-                response.writeHead(400, { "content-type": mimeTypes[".txt"] });
-                response.end("bad request: invalid JSON");
-                return;
-            }
-        });
-        return;
-    }
-    response.writeHead(404);
-    response.end();
-    return;
-}
-
-function wake(macAddress: string) {
-    macAddress = macAddress.replaceAll(":", "").replaceAll("-", "");
-    let macAddressBytes: number[] = [];
-    for (let index = 0; index < macAddress.length; index += 2) {
-        macAddressBytes.push(Number.parseInt(macAddress.slice(index, index + 2), 16));
-    }
-
-    const magicPacket = Buffer.alloc(102);
-    magicPacket.fill(0xff, 0, 6);
-    for (let index = 6; index < 102; index++) {
-        magicPacket.writeUInt8(macAddressBytes[index % 6]!, index);
-    }
-
-    const socket = dgram.createSocket("udp4");
-    socket.on("error", (error) => {
-        console.error(error);
-        socket.close();
     });
-    socket.on("connect", () => {
-        socket.send(magicPacket, 0, magicPacket.length);
-        console.log(`magic packet sent to ${macAddress}`);
-        socket.close();
-    });
-    socket.connect(9, "255.255.255.255");
-}
-
-function ping(hostname: string): boolean {
-    console.log(`ping ${hostname}`);
-    const pingCommand = process.platform == "win32" ? "ping -n 1" : "ping -c 1";
-    try {
-        execSync(`${pingCommand} ${hostname}`, { timeout: pingTimeout });
-        return true;
-    }
-    catch (error) {
-        return false;
-    }
 }
